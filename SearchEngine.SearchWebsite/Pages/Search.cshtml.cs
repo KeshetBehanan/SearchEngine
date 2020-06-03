3ed9d993ae2b558649df5dfbb3e6b53a111ba95b
KeshetBehanan <talisraeli225@gmail.com>
2020-06-03 13:57:07 +0300

    Fixed the results

--- SearchEngine.SearchWebsite/Pages/Search.cshtml.cs
+++ SearchEngine.SearchWebsite/Pages/Search.cshtml.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using SearchEngine.Data;
 
 namespace SearchEngine.SearchWebsite.Pages
 {
     public class SearchModel : PageModel
     {
+
+        private const float NORMALIZED_RATIO = 2f;
+
         #region Input Properties
 
         /// <summary>
@@ -101,37 +106,115 @@ namespace SearchEngine.SearchWebsite.Pages
             sw.Start();
 
             var stemmer = new PorterStemmer();
-            var terms = Query.Split(' ').Select(x => stemmer.StemWord(x)).Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
-            if(terms.Length == 0)
+            var input = Query.Split().Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
+            var terms = new List<string>(input.Count);
+
+            while(input.Count > 0)
+            {
+                var w1 = input.First();
+                var w2 = w1.ToLower();
+
+                if(w2.EndsWith("n't"))
+                {
+                    w1 = Regex.Replace(w1, @"n't", "", RegexOptions.IgnoreCase);
+                    w2 = w2.Replace("n't", "");
+                    if(!terms.Contains("not"))
+                        terms.Add("not");
+                }
+                else if(w2.EndsWith("'ll"))
+                {
+                    w1 = Regex.Replace(w1, @"'ll", "", RegexOptions.IgnoreCase);
+                    w2 = w2.Replace("'ll", "");
+                    if(!terms.Contains("be"))
+                        terms.Add("be");
+                }
+                else if(w2.EndsWith("'re"))
+                {
+                    w1 = Regex.Replace(w1, @"'re", "", RegexOptions.IgnoreCase);
+                    w2 = w2.Replace("'re", "");
+                    if(!terms.Contains("be"))
+                        terms.Add("be");
+                }
+                else if(w2 == "i'm" || w2 == "he's" || w2 == "she's" || w2 == "it's" ||
+                    w2 == "what's" || w2 == "when's" || w2 == "where's" || w2 == "how's" || w2 == "what's" || w2 == "why's")
+                {
+                    w1 = Regex.Replace(w1, @"'s|'m", "", RegexOptions.IgnoreCase);
+                    w2 = w2.Replace("'s", "").Replace("'m", "");
+                    if(!terms.Contains("be"))
+                        terms.Add("be");
+                }
+                else if(w2.EndsWith("'s") || w2.EndsWith("s'"))
+                {
+                    w1 = Regex.Replace(w1, @"('s)|(s')$", "", RegexOptions.IgnoreCase);
+                }
+                if(w2 == "am" || w2 == "is" || w2 == "are" || w2 == "will" || w2 == "was" || w2 == "were" || w2 == "been" || w2 == "wo")
+                    w1 = "be";
+
+                string w3 = null;
+
+                try
+                {
+                    w3 = stemmer.StemWord(w1);
+                }
+                catch { }
+
+                if(!string.IsNullOrWhiteSpace(w3) && !terms.Contains(w3))
+                    terms.Add(w3);
+
+                input.RemoveAt(0);
+            }
+            if(terms.Count == 0)
             {
                 return Redirect("/");
             }
 
-            var results = dataHelper.Keywords.Where(x => x.RootKeywordForm == terms[0]).SelectMany(x => x.KeywordWebpageRecords)
-                .Select(x => new ResultRecord()
+            var normalizedTerms = terms.Where(x => x != x.ToLower()).Select(x => x.ToLower()).ToList();
+
+            var results1 = dataHelper.Keywords.Where(x => terms.Contains(x.RootKeywordForm)).SelectMany(x => x.KeywordWebpageRecords).ToList().GroupBy(x => x.WebpageId)
+                .Select(x => new
                 {
-                    Url = x.Webpage.Url.AbsoluteUri,
-                    Title = x.Webpage.Metadata.Title,
-                    Description = x.Webpage.Metadata.Description,
-                    Score = x.Score
+                    WebpageId = x.Key,
+                    Score = x.Sum(y => y.Score) * NORMALIZED_RATIO
                 });
 
+            var results2 = dataHelper.Keywords.Where(x => normalizedTerms.Contains(x.RootKeywordForm)).SelectMany(x => x.KeywordWebpageRecords).ToList().GroupBy(x => x.WebpageId)
+                .Select(x => new
+                {
+                    WebpageId = x.Key,
+                    Score = (float)x.Sum(y => y.Score)
+                });
+
+            var results = results1.Union(results2).GroupBy(x => x.WebpageId);
+
             TotalResults = results.Count();
-            Results = results.OrderByDescending(x => x.Score).Skip((ResultPageNumber - 1) * NUMBER_OF_RESULTS_PER_PAGE).Take(NUMBER_OF_RESULTS_PER_PAGE).ToList();
+            Results = results.Select(x => new 
+            { 
+                WebpageData = GetWebpageData(x.Key),
+                Score = x.Sum(y => y.Score)
+            }).OrderByDescending(x => x.Score).Skip((ResultPageNumber - 1) * NUMBER_OF_RESULTS_PER_PAGE).Take(NUMBER_OF_RESULTS_PER_PAGE)
+            .Select(x => new ResultRecord()
+            {
+                Url = x.WebpageData.Url,
+                Title = x.WebpageData.Title,
+                Description = x.WebpageData.Description,
+                Score = x.Score
+            }).ToList();
 
             sw.Stop();
             TimeToLoad = sw.Elapsed;
             return Page();
         }
 
-        private static bool IsBelongs(Keyword x, string[] terms, bool normlize)
+        /// <summary>
+        /// Gets the data of the webpage.
+        /// </summary>
+        /// <param name="id">The ID of the webpage.</param>
+        /// <returns>The URL, title and description of the webpage.</returns>
+        private (string Url, string Title, string Description) GetWebpageData(int id)
         {
-            foreach(var term in terms)
-            {
-                if((normlize ? x.RootKeywordForm.ToLower() : x.RootKeywordForm) == term)
-                    return true;
-            }
-            return false;
+            var webpage = dataHelper.Index.Include(x => x.Metadata).First(x => id == x.Id);
+            //var metadata = dataHelper.Index.First(x => id == x.Id).Metadata;
+            return (webpage.Url.AbsoluteUri, webpage.Metadata?.Title, webpage.Metadata?.Description);
         }
     }
 }
