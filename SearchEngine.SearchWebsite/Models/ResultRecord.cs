dca3aec4953ff001db4c676cca6c4b67dbab8d11
KeshetBehanan <talisraeli225@gmail.com>
2020-06-03 13:56:25 +0300

    Changed Score to float

--- SearchEngine.SearchWebsite/Models/ResultRecord.cs
+++ SearchEngine.SearchWebsite/Models/ResultRecord.cs
@@ -5,7 +5,7 @@
         public string Title { get; set; }
         public string Url { get; set; }
         public string Description { get; set; }
-        public int Score { get; set; }
+        public float Score { get; set; }
 
     }
 }
